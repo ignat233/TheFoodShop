fe136a4c3928b235b0010c185ecc3a77a53da7c9
ignat233 <i.semaev@gmail.com>
2021-07-15 02:55:53 +0300

    FS-5.1

--- src/main/java/com/netcraker/repository/UserRepository.java
+++ src/main/java/com/netcraker/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.netcraker.repository;
 
 import com.netcraker.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
